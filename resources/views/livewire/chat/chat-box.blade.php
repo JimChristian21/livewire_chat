0df8c3d96fd7e0ebc958d66f06ea1b2d8c750da7
Jim Christian Edullantes <jimchristianedullantes9@gmail.com>
2025-01-13 00:31:14 +0800

    Added chatbox header

--- resources/views/livewire/chat/chat-box.blade.php
+++ resources/views/livewire/chat/chat-box.blade.php
@@ -1,3 +1,17 @@
 <div>
-    Chatbox
+    <header class="w-full sticky inset-x-0 flex pb-[5px] top-0 z-10 bg-white border-b">
+        <div class="flex w-full items-center px-2 lg:px-4 gap-2 md:gap-5">
+            <a href="#" class="shrink-0 lg:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+                </svg>
+            </a>
+            <div class="shrink-0 mt-2">
+                <x-avatar class="w-9 h-9 lg:w-11 lg:h-11"/>
+            </div>
+
+            <h6 class="font-bold truncate">{{fake()->name}}</h6>
+
+        </div>
+    </header>
 </div>
